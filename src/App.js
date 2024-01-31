03a81bf88ed6587695385656f153ac2f8acb3eb0
archiecheng <archiechengice@outlook.com>
2024-01-31 12:16:13 -0800

    【功能开发】学习项目初始化

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+// 项目的根组件
+// App -> index.js -> public/index.html(root)
+function getName() {
+  return 'Archie'
+}
+const count = 100
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      this is App
+      {/* 1. 使用引号传递字符串 */}
+      {'this a message'}
+      {/* 2. 识别js变量 */}
+      {count}
+      {/* 3. 函数调用 */}
+      {getName()}
+      {/* 4. 方法调用 */}
+      {new Date().getDate()}
+      {/* 5. 使用JavaScript对象 */}
+      <div style={{color:'red'}}>this is a div tag</div>
     </div>
   );
 }
