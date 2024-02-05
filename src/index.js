94a68b969f8690ed31d7a06a84b641225505cfd8
archiecheng <archiechengice@outlook.com>
2024-02-04 18:02:23 -0800

    React Router快速开始

--- src/index.js
+++ src/index.js
@@ -8,12 +8,25 @@ import App from './App';
 
 import store from './store';
 import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+// 1. 创建 router 实例对象并且配置路由对应关系
+ const router = createBrowserRouter([
+  {
+    path:'/login',
+    element: <div>我是登录</div>
+  },
+  {
+    path:'/article',
+    element: <div>我是文章页</div>
+  }
+ ])
 // 把App根组件渲染到id为root的dom节点上
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router}></RouterProvider>
   </Provider>
 );
 
